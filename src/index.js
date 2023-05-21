80e0cb8d10721ad42e8b6f0a6e73aebeba864083
UTSAV SINGH <122344389+Utsav863@users.noreply.github.com>
2023-05-21 18:39:49 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>welcome</h1>
+    <ul>
+      <li>hy</li>
+      <li>by</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
